4db920add480b8909023cb631c22d3171354292d
cassliya2307 <haliyakareem23@gmail.com>
2025-09-26 11:52:33 +0100

    Add TaskOne.java file

---
+++ Gate One/TaskOne.java
@@ -0,0 +1,30 @@
+public class TaskOne{
+
+public static void main(String[] args){
+
+
+/*int count = 1;
+for( ;count <= 10 ;count+=1){
+
+System.out.println(count);
+
+}
+*/
+
+int count = 0;
+while(count <= 9){
+count+=1;
+
+System.out.println(count);
+
+
+}
+
+
+}
+
+
+
+
+
+}
