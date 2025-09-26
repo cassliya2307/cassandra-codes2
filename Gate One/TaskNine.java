cbe0150b6df4aed6df948e3b9898bfea0b3661f8
cassliya2307 <haliyakareem23@gmail.com>
2025-09-26 12:08:55 +0100

    Add TaskNine.java file

---
+++ Gate One/TaskNine.java
@@ -0,0 +1,45 @@
+public class TaskNine{
+
+public static void main(String[] args){
+
+int sum3 = 0;
+int square = 0;
+
+
+int sum = 0;
+int count = 1;
+int num = 1;
+while(count <= 5){
+
+num = num * 4;
+sum += num;
+count++;
+
+}
+
+
+
+
+
+
+int sum2 = 0;
+int count2 = 1;
+int num2 = 1;
+while(count2 <= 5){
+
+num2 = num2 * 8;
+sum2 += num2;
+count2++;
+
+}
+
+sum3 = sum + sum2;
+square = sum3 * sum3;
+System.out.print(square);
+
+}
+
+
+
+
+}
