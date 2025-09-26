9036e00dd8ffdb26d8df81680f6e0d3271a40f7c
cassliya2307 <haliyakareem23@gmail.com>
2025-09-26 12:05:12 +0100

    Add TaskFive.java file

---
+++ Gate One/TaskFive.java
@@ -0,0 +1,33 @@
+public class TaskFive{
+
+public static void main(String[] args){
+
+
+int count = 4;
+int num = 4;
+int num2 = 8;
+for( ;count <= 8  ;count++){
+
+System.out.print(num  );
+
+
+}
+
+
+int count2 = 1;
+while(count2 <= 5){
+count2++;
+System.out.print(  num2);
+
+
+}
+
+
+
+
+}
+
+
+
+
+}
