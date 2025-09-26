4895a07363fd235325b85f841fc172207ce54b31
cassliya2307 <haliyakareem23@gmail.com>
2025-09-26 12:07:07 +0100

    Add TaskSeven.java file

---
+++ Gate One/TaskSeven.java
@@ -0,0 +1,40 @@
+public class TaskSeven{
+
+public static void main(String[] args){
+
+int sum = 0;
+int count = 1;
+int num = 1;
+while(count <= 5){
+
+num = num * 4;
+sum += num;
+count++;
+
+}
+System.out.print(sum);
+
+
+
+
+
+int sum2 = 0;
+int count2 = 1;
+int num2 = 1;
+while(count2 <= 5){
+
+num2 = num2 * 8;
+sum2 += num2;
+count2++;
+
+}
+System.out.print(" " + sum2);
+
+
+
+}
+
+
+
+
+}
