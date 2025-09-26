a1c9231a4d10c8511cc3c56b76aec1bf65479824
cassliya2307 <haliyakareem23@gmail.com>
2025-09-26 12:05:56 +0100

    Add TaskSix.java file

---
+++ Gate One/TaskSix.java
@@ -0,0 +1,39 @@
+public class TaskSix{
+
+public static void main(String[] args){
+
+
+int count = 1;
+int num = 1;
+while(count <= 5){
+count++;
+num = num * 4;
+System.out.print(" " + num);
+
+
+}
+
+
+
+
+
+
+int count2 = 1;
+int num2 = 1;
+while(count2 <= 5){
+count2++;
+num2 = num2 * 8;
+System.out.print(" " + num2);
+
+
+}
+
+
+
+
+}
+
+
+
+
+}
