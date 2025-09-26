ded220668e0366e4b81dedfd3a903ea455794cc3
cassliya2307 <haliyakareem23@gmail.com>
2025-09-26 12:01:22 +0100

    Add TaskTwo.java file

---
+++ Gate One/TaskTwo.java
@@ -0,0 +1,32 @@
+public class TaskTwo{
+
+public static void main(String[] args){
+
+
+/*
+int count = 0;
+for( ;count <= 10 ;count+=2){
+
+System.out.println(count);
+
+}
+*/
+
+int count = 0;
+while(count <= 10){
+count+=1;
+System.out.println(count);
+
+
+
+}
+
+
+
+
+}
+
+
+
+
+}
