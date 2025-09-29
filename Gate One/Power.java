6b81b80c922ede27e6920f5e8e44b72d77b4e173
cassliya2307 <haliyakareem23@gmail.com>
2025-09-29 08:53:35 +0100

    Add Power.java file

---
+++ Gate One/Power.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Power{
+
+public static void main(Strings[] args){
+
+System.out.print("Enter base number: ");
+int base = scanner.nextInt();
+
+System.out.print("Enter power: ");
+int power = scanner.nextInt();
+
+int result = 1;
+
+for (int count = 1; count <= power; count++) {
+result = result * base;
+}
+
+System.out.println(base + " raised to power " + power + " is " + result);
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+}
