678ddb2183ed8e3e700c1720330d522e02c24ab2
cassliya2307 <haliyakareem23@gmail.com>
2025-09-29 08:52:43 +0100

    Add Factorial.java file

---
+++ Gate One/Factorial.java
@@ -0,0 +1,31 @@
+public class Factorial{
+
+public static void main(String[] args){
+
+
+System.out.print("Enter a number: ");
+int number = scanner.nextInt();
+
+int factorial = 1;
+
+for(int count = 1; count <= number; count++){
+
+	factorial *= count;
+
+
+
+
+
+}
+
+System.out.print(factorial);
+
+
+
+
+
+}
+
+
+
+}
